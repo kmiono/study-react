22df33a359e24b106ec9f9740945da8d20952d25
kmiono <86765332+kmiono@users.noreply.github.com>
2023-05-10 15:57:31 +0900

    ✨ コンポーネント新規作成

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer>
+      <a href="/" target="_blank" rel="noopener noreferrer">
+        Powerd by{""}
+        <img src="/vercel.svg" alt="Vercel Logo" />
+      </a>
+    </footer>
+  );
+}
